5cc5fcea37864ae3f0946ef719817db7fcdf704d
Carlos Pavanetti <carlos.pavanetti@buser.com.br>
2022-01-12 20:35:42 -0300

    Handle errors from run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -13,7 +14,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config)
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -34,9 +39,9 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename) //
-        .expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents)
+    println!("With text:\n{}", contents);
+    Ok(())
 }
