3b606ee1a7e6cf6dd2cb36bcc5a2591e006b3b01
Carlos Pavanetti <carlos.pavanetti@buser.com.br>
2022-01-13 13:49:40 -0300

    Define search_case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,18 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+  results
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -63,5 +75,20 @@ safe, fast, productive.
 Pick three.";
 
     assert_eq!(vec![] as Vec<&str>, search(query, contents));
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+      vec!["Rust:", "Trust me."],
+      search_case_insensitive(query, contents)
+    );
   }
 }
